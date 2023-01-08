46298013275b1cba86015c0caa6ea9b0b3723c98
arif009xd <syarifhdyt009xd@gmail.com>
2023-01-08 15:51:52 +0700

    Membuat Entity

---
+++ src/main/java/com/example/application/data/entity/Produk.java
@@ -0,0 +1,62 @@
+package com.example.application.data.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Produk extends AbstractEntity{
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public String getNamaKategori() {
+        return namaKategori;
+    }
+
+    public void setNamaKategori(String namaKategori) {
+        this.namaKategori = namaKategori;
+    }
+
+    public byte[] getThumbnails() {
+        return Thumbnails;
+    }
+
+    public void setThumbnails(byte[] thumbnails) {
+        Thumbnails = thumbnails;
+    }
+
+    public String getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(String harga) {
+        Harga = harga;
+    }
+
+    public String getTerjual() {
+        return Terjual;
+    }
+
+    public void setTerjual(String terjual) {
+        Terjual = terjual;
+    }
+
+    public String getDiBuat() {
+        return diBuat;
+    }
+
+    public void setDiBuat(String diBuat) {
+        this.diBuat = diBuat;
+    }
+
+    private  String namaProduk;
+    private String namaKategori;
+    private  byte[] Thumbnails;
+    private  String Harga;
+    private  String Terjual;
+    private  String diBuat;
+}
